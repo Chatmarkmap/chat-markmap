4225c529f541090d55cde90755545246537d1ad8
Cygra <sjtuwbh@gmail.com>
2025-02-26 23:49:33 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,148 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useMemo, useRef } from "react";
+
+import { useChat } from "@ai-sdk/react";
+import { Button, Textarea, Tooltip } from "@heroui/react";
+import { CircleX, CornerDownLeft } from "lucide-react";
+import { Transformer } from "markmap-lib";
+import { Markmap } from "markmap-view";
+import ReactMarkdown from "react-markdown";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+
+import { cn } from "@/lib/utils";
+
+const transformer = new Transformer();
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { messages, input, setInput, append, status, stop } = useChat();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const chatScrollRef = useRef<HTMLDivElement>(null);
+  const refSvg = useRef<SVGSVGElement>(null);
+  const refMarkmap = useRef<Markmap>(null);
+
+  useEffect(() => {
+    if (chatScrollRef.current) {
+      chatScrollRef.current.scrollTop = chatScrollRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  const lastContent = useMemo(() => {
+    return messages.findLast((message) => message.role !== "user");
+  }, [messages]);
+
+  useEffect(() => {
+    if (refMarkmap.current) return;
+    const markmap = Markmap.create(refSvg.current);
+    refMarkmap.current = markmap;
+  }, [refSvg.current]);
+
+  useEffect(() => {
+    const markmap = refMarkmap.current;
+    if (lastContent?.content && markmap) {
+      const { root } = transformer.transform(lastContent.content);
+      markmap.setData(root).then(() => {
+        markmap.fit();
+      });
+    }
+  }, [lastContent?.content]);
+
+  const onSubmit = () => {
+    if (!/\S/.test(input)) return;
+    append({
+      role: "user",
+      content: input,
+    });
+    setInput("");
+  };
+
+  const isLoading = status === "streaming" || status === "submitted";
+
+  return (
+    <div className="h-[100vh] w-full overflow-hidden p-2">
+      <PanelGroup direction="horizontal">
+        <Panel id={"sidepanel"} defaultSize={30} minSize={20} order={1}>
+          <div
+            className="relative mb-2 mr-1 flex h-full flex-col items-stretch
+              overflow-hidden rounded bg-white"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <div
+              className="flex-1 overflow-y-auto overflow-x-hidden"
+              ref={chatScrollRef}
+            >
+              <div className="flex flex-col p-2">
+                {messages.map(({ id, role, content }) => {
+                  const isUser = role === "user";
+                  return (
+                    <div
+                      key={id}
+                      className={cn("mb-3 flex self-stretch", {
+                        "self-end pl-6": isUser,
+                        "self-start pr-6": !isUser,
+                      })}
+                    >
+                      <div
+                        className={cn("rounded px-2", { "bg-muted": isUser })}
+                      >
+                        {isUser ? <div className="p-2">{content}</div> : 
+                          <ReactMarkdown className={"markdown"}>
+                            {content}
+                          </ReactMarkdown>
+                        }
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="space-y-2 p-2">
+              <div className="flex items-end space-x-2">
+                <Textarea
+                  value={input}
+                  onValueChange={setInput}
+                  className={"flex-1"}
+                  size={"sm"}
+                  minRows={1}
+                  maxRows={10}
+                />
+                <Tooltip content={isLoading ? "Thinking" : "Submit"}>
+                  <Button
+                    size={"sm"}
+                    color={"primary"}
+                    onPress={onSubmit}
+                    isDisabled={!input}
+                    isLoading={isLoading}
+                    isIconOnly
+                  >
+                    <CornerDownLeft />
+                  </Button>
+                </Tooltip>
+                {isLoading ? (
+                  <Tooltip content={"Stop"}>
+                    <Button
+                      size={"sm"}
+                      isIconOnly
+                      variant="light"
+                      className="rounded-full"
+                      onPress={stop}
+                    >
+                      <CircleX />
+                    </Button>
+                  </Tooltip>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </Panel>
+        <PanelResizeHandle
+          className="transition-background mr-1 w-1 rounded bg-[#999]
+            hover:bg-[#333]"
+        />
+        <Panel defaultSize={70}  minSize={50} order={2}>
+          <svg ref={refSvg} className="h-full w-full"></svg>
+        </Panel>
+      </PanelGroup>
     </div>
   );
 }
